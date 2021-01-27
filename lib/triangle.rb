38adeb54f3aa5ae0ea08a3d44694522cbdc3cd25
Nate <7nater.nelson@gmail.com>
2021-01-27 14:23:04 -0700

    commit tree cd9ec09f5a177a8fa08dc93a9ea958c934c0f14d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(s1,s2,s3)
+    self.side1 = s1
+    self.side2 = s2
+    self.side3 = s3
+  end
+
+  def kind
+    sides = [side1,side2,side3].sort
+    side_check = (sides[0] + sides[1]) > sides[2] && !sides.include?(0) #boolean
+    # binding.pry
+    if side_check == false
+      begin
+        raise TriangleError
+      end
+    end
+    
+    return :scalene if sides.uniq.count == 3
+    return :isosceles if sides.uniq.count == 2
+    return :equilateral if sides.uniq.count == 1
+    
+  end
+  class TriangleError < StandardError
+
+  end
 end
